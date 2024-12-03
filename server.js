73e938a695e80b812a4ad3506f7a997abcc3ac8a
Heorhii Manzhai <georgii.ua@gmail.com>
2024-12-03 22:24:46 +0200

    Port issue

--- server.js
+++ server.js
@@ -900,6 +900,10 @@ app.get("/api/admin/group-students", (req, res) => {
     });
 });
 
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+//app.listen(5000, () => console.log("Server running on http://localhost:5000"));
